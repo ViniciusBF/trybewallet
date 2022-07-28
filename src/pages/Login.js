945a4883f689bd94eb30fa259308d7043e8b3391
Vinícius <viniciusgolfera@gmail.com>
2022-07-28 17:34:53 -0300

    Requisito 1 atendido

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,78 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { func, shape } from 'prop-types';
+import { userAction } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      valid: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({ [name]: value }, this.validar);
+  }
+
+  validar = () => {
+    const { email, password } = this.state;
+    const NUMBER_VALID = 5;
+    const valid1 = password.length > NUMBER_VALID;
+    const valid2 = email.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/);
+    this.setState({ valid: [valid1, valid2].every((e) => e) });
+  }
+
+  hadleClick = async () => {
+    const { saveEmail, history } = this.props;
+    const { email } = this.state;
+    await saveEmail({ email });
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, valid } = this.state;
+    return (
+      <div>
+        <input
+          type="email"
+          data-testid="email-input"
+          value={ email }
+          name="email"
+          onChange={ this.handleChange }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          value={ password }
+          name="password"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          disabled={ !valid }
+          onClick={ this.hadleClick }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  saveEmail: (state) => dispatch(userAction(state)),
+});
+
+Login.propTypes = {
+  saveEmail: func,
+  history: shape({
+    push: func,
+  }),
+}.isRequired;
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,21 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { WALLET_ACTION } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  editor: false,
+  idToEdit: 0,
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case WALLET_ACTION:
+    return {
+      ...action.payload,
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
