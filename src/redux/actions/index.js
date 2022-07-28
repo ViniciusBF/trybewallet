7c958827976763cdd2cfe901f93cfddd6c64bd91
Vinícius <viniciusgolfera@gmail.com>
2022-07-28 17:34:53 -0300

    Requisito 1 atendido

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,7 @@
-// Coloque aqui suas actions
+const WALLET_ACTION = 'WALLET_ACTION';
+const USER_ACTION = 'USER_ACTION';
+
+const walletAction = (payload) => ({ type: WALLET_ACTION, payload });
+const userAction = (payload) => ({ type: USER_ACTION, payload });
+
+export { walletAction, userAction, WALLET_ACTION, USER_ACTION };
